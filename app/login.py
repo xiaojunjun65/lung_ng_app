16eb22f8585d97183f94559a8d12c2d64b24d596
zhouguojun <zhouguojun@extrotec.com>
2023-05-16 18:26:42 +0800

    230516

---
+++ app/login.py
@@ -0,0 +1,114 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, \
+    QMessageBox, QComboBox
+from PyQt5.QtGui import QColor, QPalette, QFont
+from PyQt5.QtCore import Qt
+import pandas as pd
+
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("登录")
+        self.resize(400, 200)
+
+        # 设置颜色样式
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(200, 230, 255))
+        self.setPalette(palette)
+
+        # 添加黑色标题
+        title_label = QLabel("肺炎诊断系统", self)
+        title_label.setAlignment(Qt.AlignCenter)
+        title_label.setFont(QFont("Arial", 18, QFont.Bold))
+        title_label.setStyleSheet("color: black;")
+
+        # 创建控件
+        self.username_label = QLabel("用户名:")
+        self.password_label = QLabel("密码:")
+        self.username_input = QLineEdit()
+        self.password_input = QLineEdit()
+        self.password_input.setEchoMode(QLineEdit.Password)
+        self.login_button = QPushButton("登录")
+        self.register_button = QPushButton("注册")
+        self.user_type_combobox = QComboBox()
+        self.user_type_combobox.addItems(["用户", "管理员"])
+
+        # 设置CSS样式
+        self.setStyleSheet("""
+            QLabel {
+                font-size: 16px;
+                color: #333333;
+            }
+            QLineEdit, QComboBox {
+                font-size: 14px;
+            }
+            QPushButton {
+                font-size: 14px;
+                padding: 8px 16px;
+                background-color: #4CAF50;
+                color: #FFFFFF;
+                border: none;
+            }
+            QPushButton:hover {
+                background-color: #45a049;
+            }
+        """)
+
+        # 布局
+        layout = QVBoxLayout()
+        layout.addWidget(title_label)
+        layout.addWidget(self.username_label)
+        layout.addWidget(self.username_input)
+        layout.addWidget(self.password_label)
+        layout.addWidget(self.password_input)
+        layout.addWidget(self.user_type_combobox)
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.login_button)
+        button_layout.addWidget(self.register_button)
+
+        layout.addLayout(button_layout)
+        layout.addStretch()  # 添加一个弹性空间，使布局居中显示
+        self.setLayout(layout)
+
+        # 绑定按钮点击事件
+        self.login_button.clicked.connect(self.login)
+        self.register_button.clicked.connect(self.register)
+
+    def login(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+        user_type = self.user_type_combobox.currentText()
+
+        # 使用 pandas 读取 "db.xlsx" 文件
+        try:
+            df = pd.read_excel("db.xlsx")
+            yh_column = df["yh"]
+            ps_column = df["ps"]
+            # 判断用户名是否存在
+            if username in yh_column.values:
+
+                # 获取对应用户名的密码
+                correct_password = ps_column[yh_column[yh_column == username].index[0]]
+
+                if str(password) == str(correct_password):
+                    QMessageBox.information(self, "登录结果", f"{user_type} {username} 登录成功")
+                    return
+            QMessageBox.warning(self, "登录结果", "暂无用户")
+        except Exception as e:
+            QMessageBox.warning(self, "登录结果", f"登录失败：{str(e)}")
+
+    def register(self):
+        # 在这里编写注册逻辑
+        from register import RegisterWindow
+        self.register_window = RegisterWindow()
+        self.register_window.show()
+        self.hide()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = LoginWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ app/register.py
@@ -0,0 +1,96 @@
+import sys
+import pandas as pd
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QColor, QPalette
+import openpyxl
+import os.path
+from login import  LoginWindow
+
+class RegisterWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("注册")
+        self.resize(400, 200)
+
+        # 设置颜色样式
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(200, 230, 255))
+        self.setPalette(palette)
+
+        # 创建控件
+        self.username_label = QLabel("用户名:")
+        self.password_label = QLabel("密码:")
+        self.username_input = QLineEdit()
+        self.password_input = QLineEdit()
+        self.password_input.setEchoMode(QLineEdit.Password)
+        self.register_button = QPushButton("注册")
+
+        # 设置CSS样式
+        self.setStyleSheet("""
+            QLabel {
+                font-size: 16px;
+                color: #333333;
+            }
+            QLineEdit {
+                font-size: 14px;
+            }
+            QPushButton {
+                font-size: 14px;
+                padding: 8px 16px;
+                background-color: #4CAF50;
+                color: #FFFFFF;
+                border: none;
+            }
+            QPushButton:hover {
+                background-color: #45a049;
+            }
+        """)
+
+        # 布局
+        layout = QVBoxLayout()
+        layout.addWidget(self.username_label)
+        layout.addWidget(self.username_input)
+        layout.addWidget(self.password_label)
+        layout.addWidget(self.password_input)
+        layout.addWidget(self.register_button)
+        layout.addStretch()  # 添加一个弹性空间，使布局居中显示
+        self.setLayout(layout)
+
+        # 绑定按钮点击事件
+        self.register_button.clicked.connect(self.register)
+
+    import pandas as pd
+
+    def register(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+
+
+        # 检查密码是否为空
+        if not password:
+            QMessageBox.warning(self, "注册结果", "密码不能为空")
+            return
+
+        # 在这里编写注册逻辑
+        data = {'yh': [username], 'ps': [password]}
+        df = pd.DataFrame(data)
+
+        try:
+            original_data = pd.read_excel('db.xlsx')
+            save_data = original_data.append(df)
+
+            save_data.to_excel('db.xlsx', index=False)
+
+            QMessageBox.information(self, "注册结果", "注册成功")
+            self.LoginWindow = LoginWindow()
+            self.LoginWindow.show()
+            self.hide()
+        except Exception as e:
+            QMessageBox.warning(self, "注册结果", f"注册失败：{str(e)}")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = RegisterWindow()
+    window.show()
+    sys.exit(app.exec_())
